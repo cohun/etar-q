923feac9e4ab104da2a261f8da48043a03d52cb1
Colin Hunwald <colin.hunwald@gmail.com>
2023-09-07 10:56:07 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:etar_q/src/routing/app_router.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+    return MaterialApp.router(
+      routerConfig: goRouter,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+    );
+  }
+}
