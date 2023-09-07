0dda91b45c5b3f696d92746ad5f2c1673fc79992
Colin Hunwald <colin.hunwald@gmail.com>
2023-09-07 14:56:55 +0200

    Andrea 7

--- lib/src/screens/custom_profile_screen.dart
+++ lib/src/screens/custom_profile_screen.dart
@@ -1,15 +1,25 @@
+import 'package:etar_q/src/routing/app_router.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomProfileScreen extends ConsumerWidget {
   const CustomProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
+    final authProviders = [EmailAuthProvider()];
+    return ProfileScreen(
       appBar: AppBar(
-        title: const Text('Profile'),
+        title: const Text('Felhasználói profil'),
       ),
+      providers: authProviders,
+      actions: [
+        SignedOutAction((context) {
+          context.goNamed(AppRoute.signIn.name);
+        }),
+      ],
     );
   }
 }
--- lib/src/screens/custom_sign_in_screen.dart
+++ lib/src/screens/custom_sign_in_screen.dart
@@ -1,14 +1,64 @@
+import 'package:etar_q/src/routing/app_router.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSignInScreen extends ConsumerWidget {
   const CustomSignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final authProviders = [EmailAuthProvider()];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign in'),
+        title:  Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/ETAR.jpg', height: 40,),
+            const SizedBox(width: 12,),
+            const Text('Bejelentkezés'),
+          ],
+        ),
+      ),
+      body: SignInScreen(
+        providers: authProviders,
+        actions: [
+          AuthStateChangeAction<SignedIn>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          }),
+          AuthStateChangeAction<UserCreated>((context, state) {
+            context.goNamed(AppRoute.profile.name);
+          }),
+        ],
+        headerBuilder: (context, constraints, shrinkOffset
+) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('assets/images/image.jpg'),
+               ),
+             );
+           },
+           footerBuilder: (context, action) {
+             return const Padding(
+               padding: EdgeInsets.only(top: 16),
+               child: Text(
+                 'Bejelentkezés esetén elfogadod a használati feltételeket és az adatvédelmi szabályzatot.',
+                 style: TextStyle(color: Colors.grey),
+               ),
+             );
+           },
+           sideBuilder: (context, shrinkOffset) {
+             return Padding(
+               padding: const EdgeInsets.all(20),
+               child: AspectRatio(
+                 aspectRatio: 1,
+                 child: Image.asset('assets/images/image.jpg'),
+               ),
+             );
+           },
       ),
     );
   }
