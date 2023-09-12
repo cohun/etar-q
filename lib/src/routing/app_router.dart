c9d2fa9f4c25dde4bcbbb9fcd2727c6e30853b4d
Colin Hunwald <colin.hunwald@gmail.com>
2023-09-12 15:21:42 +0200

    Andrea 7

--- lib/src/routing/app_router.dart
+++ lib/src/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:etar_q/src/screens/custom_profile_screen.dart';
 import 'package:etar_q/src/screens/custom_sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
@@ -13,6 +14,19 @@ final goRouterProvider = Provider<GoRouter>((ref) {
   return GoRouter(
     initialLocation: '/sign-in',
     debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+      if (isLoggedIn) {
+          if (state.uri.path == '/sign-in') {
+            return '/profile';
+          }
+         } else {
+          if (state.uri.path == '/profile') {
+            return '/sign-in';
+          }
+        }
+        return null;  
+    },
     routes: [
       GoRoute(
         path: '/sign-in',
