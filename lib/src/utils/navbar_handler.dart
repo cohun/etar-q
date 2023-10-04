a21c454d19e08b8114e44859d0e3d6f29b87d93b
cohun <colin.hunwald@gmail.com>
2023-10-04 19:40:15 +0200

    BottomNavbar only if approved user

--- lib/src/utils/navbar_handler.dart
+++ lib/src/utils/navbar_handler.dart
@@ -1,23 +1,19 @@
+import 'package:etar_q/src/routing/app_router.dart';
 import 'package:etar_q/src/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:etar_q/src/data/firestore_repository.dart';
 
-class BottomNavigation extends StatefulWidget {
+class BottomNavigation extends ConsumerStatefulWidget {
   const BottomNavigation({super.key});
 
   @override
-  State<BottomNavigation> createState() => _BottomNavigationState();
+  ConsumerState<BottomNavigation> createState() => _BottomNavigationState();
 }
 
-class _BottomNavigationState extends State<BottomNavigation> {
+class _BottomNavigationState extends ConsumerState<BottomNavigation> {
   int _selectedIndex = 0;
-  bool isVisible = false;
-  setBottombarVisible(String approvedRole) {
-    if (approvedRole != 'elbírálás alatt') {
-      setState(() {
-        isVisible = true;
-      });
-    }
-  }
 
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
@@ -37,42 +33,50 @@ class _BottomNavigationState extends State<BottomNavigation> {
     ),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    final firestoreRepository = ref.watch(firestoreRepositoryProvider);
+    final user = ref.read(firebaseAuthProvider).currentUser;
+    void tapped(int index) {
+      firestoreRepository.oneUserQuery(user!.uid).then(
+            (value) => value.approvedRole == 'elbírálás alatt'
+                ? ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text('Jelenleg nincs jogosultságod!')),
+                  )
+                : setState(() {
+                    _selectedIndex = index;
+                  }),
+          );
+    }
+
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Kezdőlap',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.precision_manufacturing),
-            label: 'Termékek',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.track_changes),
-            label: 'NFC',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.barcode_reader),
-            label: 'QR +vonalkód',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        unselectedItemColor: Colors.blueGrey,
-        onTap: _onItemTapped,
-      ),
-    );
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Kezdőlap',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.precision_manufacturing),
+              label: 'Termékek',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.track_changes),
+              label: 'NFC',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.barcode_reader),
+              label: 'QR +vonalkód',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          unselectedItemColor: Colors.blueGrey,
+          onTap: tapped,
+        ));
   }
 }
